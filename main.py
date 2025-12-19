3154175e45f5ba5b02f8289702991ebc64b6e6d6
dank11303 <danpk13@gmail.com>
2025-12-19 10:45:58 -0500

    add get_character_counts function to stats.py and implement in main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 ###imports
-from stats import get_book_text, get_word_count
+from stats import get_book_text, get_word_count, get_character_counts
 
 def main():#main function that runs the program
     print(f"Found {get_word_count(get_book_text("books/frankenstein.txt"))} total words")
+    characterCounts = get_character_counts(get_book_text("books/frankenstein.txt"))
+
+    for c in characterCounts:
+        print(f"{c}: {characterCounts[c]}")
 
 ###end main function
 
--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def get_book_text(filePath): #function that will take contents of a file and ret
 def get_word_count(contents): #function to count the number of words in the book
     return len(contents.split()) #return the length of an array of the words from the book
 ### end get_word_count(str)
+
+def get_character_counts(contents): #function to count the occurrence of each character in the book
+    characters = {} #declare empty dictionary
+
+    for c in contents: #for loop to parse each character
+        #check if the character is already in the dictionary or not and update or add accordingly
+        if c in characters:
+            characters[c.lower()] += 1
+        else:
+            characters[c.lower()] = 1
+
+    return characters #return dictionary
+###end get_character_coutns(str)
