5316d5a9bf1ea0bd9c7d4acdea83f935c6bb4228
dank11303 <danpk13@gmail.com>
2025-12-19 11:57:34 -0500

    added 2 books to book directory. updated the output look. Dynamic paths to books using sys.argv[1]

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 ###imports
 from stats import get_book_text, get_word_count, get_character_counts, sort_character_counts
+import sys
+
 
 def main():#main function that runs the program
-    print(f"Found {get_word_count(get_book_text("books/frankenstein.txt"))} total words") #print word count
-    sortedCharacterCounts = sort_character_counts(get_character_counts(get_book_text("books/frankenstein.txt"))) #get a sorted list of characters
+    #check if sys.argv has 2 entries
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(get_book_text(sys.argv[1]))} total words") #print word count
+    print("--------- Character Count -------")
+    
+    sortedCharacterCounts = sort_character_counts(get_character_counts(get_book_text(sys.argv[1]))) #get a sorted list of characters
 
     #loop through the sorted list and print only alphabetical characters
     for c in sortedCharacterCounts:
